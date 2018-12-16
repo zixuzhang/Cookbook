9377ba1c150e6a81a080afba988f471c8d3a838f
zhangzixu <zhangzixu3@163.com>
2018-12-16 13:54:13 +0800

    学习第八章

---
+++ ch08/8_2.py
@@ -0,0 +1,18 @@
+_formats = {
+    'ymd':'{d.year}-{d.month}-{d.day}',
+    'mdy':'{d.month}/{d.day}/{d.year}',
+    'dmy':'{d.day}/{d.month}/{d.year}'
+}
+
+class Date:
+    def __init__(self,year,month,day):
+        self.year = year
+        self.month = month 
+        self.day = day
+
+    def __format__(self,code):
+        if code == '':
+            code == 'ymd'
+        print(code)
+        fmt = _formats[code]
+        return fmt.format(d=self)
